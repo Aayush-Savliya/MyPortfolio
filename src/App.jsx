8b9ac0e3cf6079dcb6c7815622c495a18730df26
Aayush-Savliya <akspatel23805@gmail.com>
2025-03-08 14:59:18 +0530

    commit1

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,19 @@ import Experiance from "./Components/Experiance";
 import Footer from "./Components/Footer";
 import Contact from "./Components/Contact";
 import { Toaster } from 'react-hot-toast';
-import { Route, Routes } from "react-router-dom";
 
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/Home" element={<Home/>}/>
-        <Route path="/About" element={<About/>}/>
-        <Route path="/Portfolio" element={<Portfolio/>}/>
-        <Route path="/Experiance" element={<Experiance/>}/>
-        <Route path="/Contact" element={<Contact/>}/>
-        <Footer/>
-      </Routes>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Portfolio/>
+      <Experiance/>
+      <Contact/>
+      <Footer/>
+     
       <Toaster />
     </div>
    

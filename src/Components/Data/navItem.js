87dc92ad7a6c401a2f835bb4981cad93e94aad9f
Aayush-Savliya <akspatel23805@gmail.com>
2025-03-09 13:28:10 +0530

    object2

--- src/Components/Data/navItem.js
+++ src/Components/Data/navItem.js
@@ -1,26 +1,28 @@
- const navItems = [
-    {
-      id: 1,
-      text: "Home",
-    },
+export const navItems = [
+  {
+    id: 1,
+    text: "Home",
+  },
 
-    {
-      id: 2,
-      text: "About",
-    },
+  {
+    id: 2,
+    text: "About",
+  },
 
-    {
-      id: 3,
-      text: "Portfolio",
-    },
+  {
+    id: 3,
+    text: "Portfolio",
+  },
 
-    {
-      id: 4,
-      text: "Experiance",
-    },
+  {
+    id: 4,
+    text: "Experiance",
+  },
 
-    {
-      id: 5,
-      text: "Contact",
-    },
-  ];
+  {
+    id: 5,
+    text: "Contact",
+  },
+];
+
+export default navItems;
